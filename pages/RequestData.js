884a730177672f29d27da3930768aeade8459490
leon0x01 <ushrestha358@gmail.com>
2023-04-28 12:38:24 +0545

    Request Data and retreive data after approval

--- pages/RequestData.js
+++ pages/RequestData.js
@@ -4,7 +4,9 @@ import { getContract } from './contract';
 export default function RequestData() {
     const [dataProvider, setDataProvider] = useState('');
     const [kycField, setKycField] = useState('');
+    const [data, setData] = useState('');
     const [error, setError] = useState(null);
+    const [error1, setError1] = useState(null);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -24,8 +26,20 @@ export default function RequestData() {
             setError(err.message);
         }
     };
+    const handleRequestSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const contract = await getContract();
+      const result = await contract.getRequestedDataFromProvider(dataProvider, kycField);
+      setData(result);
+    } catch (error) {
+      setError1(error.message);
+    }
+  };
 
     return (
+        <div>
+            <h2 class="py-5 text-4xl font-bold dark:text-yellow">Request Data for approval</h2>
         <form class="w-full max-w-sm" onSubmit={handleSubmit}>
         <div class="mb-4">
             <label class="block text-gray-700 font-bold mb-2" for="inline-full-name">
@@ -44,5 +58,22 @@ export default function RequestData() {
         {error && <p class="text-red-500">{error}</p>}
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Request KYC</button>
     </form> 
+    <h2 class="py-5 text-4xl font-bold dark:text-yellow">Get Request Data From Provider</h2>
+     <form onSubmit={handleRequestSubmit}>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Get Data</button>
+      </form>
+      {data && (
+        <div>
+          <h2>Retrieved Data:</h2>
+          <p>{data}</p>
+        </div>
+      )}
+      {error1 && (
+        <div>
+          <h2>Error:</h2>
+          <p>{error1}</p>
+        </div>
+      )}
+    </div>
     );
 }
