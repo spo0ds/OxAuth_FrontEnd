330567474a1259685d7db0594dc89afd1ebd8166
leon0x01 <ushrestha358@gmail.com>
2023-04-30 09:54:11 +0545

    few design

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,67 @@
-import Head from "next/head"
-import Image from "next/image"
-import { useMoralis } from "react-moralis"
+import Head from "next/head";
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
-    const { isWeb3Enabled } = useMoralis()
-    return (
-        <div className="container mx-auto">
-            <h1 className="py-4 px-4 font-bold text-2xl">Your NFT</h1>
-            <div className="flex flex-wrap">
-                {isWeb3Enabled ? <div>Web3 Enabled</div> : <div>Web3 Currently Not Enabled</div>}
-            </div>
-        </div>
-    )
+  const { isWeb3Enabled, authenticate, isAuthenticated } = useMoralis();
+
+  const handleWalletConnect = async () => {
+    try {
+      await authenticate({ provider: "walletconnect" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <Head>
+        <title>NFT KYC Authentication</title>
+      </Head>
+      <h1 className="py-4 px-4 font-bold text-2xl">Your NFT</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          <div className="p-4 bg-green-200 rounded-md">Web3 Enabled</div>
+        ) : (
+          <div className="p-4 bg-red-200 rounded-md">
+            Web3 Currently Not Enabled
+          </div>
+        )}
+      </div>
+      <main className="flex flex-wrap mt-8">
+        <section className="w-full md:w-1/2 p-4">
+          <h1 className="font-bold text-2xl mb-4">NFT KYC Authentication</h1>
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            vel malesuada arcu. Maecenas sit amet nisi libero. Nullam in mauris
+            non nibh rutrum dictum. Fusce hendrerit felis vitae dui dictum
+            hendrerit. Integer vitae ultricies velit, in molestie nunc.
+          </p>
+          {!isAuthenticated && (
+            <button
+              onClick={handleWalletConnect}
+              className="bg-blue-500 text-white py-2 px-4 rounded-md"
+            >
+              Connect with Wallet
+            </button>
+          )}
+          {isAuthenticated && (
+            <p className="text-green-500 mt-4">Successfully authenticated</p>
+          )}
+        </section>
+        <section className="w-full md:w-1/2 p-4">
+          <Image
+            src="/photo.png"
+            alt="NFT"
+            width={500}
+            height={500}
+            className="rounded-md"
+          />
+        </section>
+      </main>
+      <footer className="mt-8 text-center">
+        <p>&copy; 2023 NFT KYC Authentication. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
 }
--- pages/nft.js
+++ pages/nft.js
@@ -188,61 +188,60 @@ export default function nft() {
     }
 
     return (
-        <div class="p-4">
-            <h1 class="text-2xl font-bold mb-4">Mint an NFT</h1>
-            {minting ? (
-                <p class="mb-4">Minting NFT...</p>
-            ) : (
-                <button
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-                    onClick={handleMint}
-                >
-                    Mint NFT
-                </button>
-            )}
-            {txHash && <p class="mb-4">Transaction Hash: {txHash}</p>}
-            {tokenId && <p class="mb-4">Token ID: {tokenId}</p>}
-            {tokenURI && (
-                <div class="mb-4">
-                    <p class="mb-2">Token URI:</p>
-                    <pre class="bg-gray-100 p-2 rounded">{tokenURI}</pre>
-                </div>
-            )}
-            {error && <p class="text-red-500 mb-4">Error: {error}</p>}
-            <h1 class="text-2xl font-bold mb-4">Burn NFT</h1>
+      <div class="p-4 bg-gray-900 text-white">
+  <h1 class="text-3xl font-bold mb-8">Mint an NFT</h1>
+  {minting ? (
+    <p class="mb-4">Minting NFT...</p>
+  ) : (
+    <button
+      class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded mb-8"
+      onClick={handleMint}
+    >
+      Mint NFT
+    </button>
+  )}
+  {txHash && <p class="mb-4">Transaction Hash: {txHash}</p>}
+  {tokenId && <p class="mb-4">Token ID: {tokenId}</p>}
+  {tokenURI && (
+    <div class="mb-4">
+      <p class="mb-2">Token URI:</p>
+      <pre class="bg-gray-800 p-2 rounded">{tokenURI}</pre>
+    </div>
+  )}
+  {error && <p class="text-red-500 mb-4">Error: {error}</p>}
+  <h1 class="text-3xl font-bold mb-8">Burn NFT</h1>
 
-            {/* Check NFT */}
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-                onClick={handleCheckNFT}
-            >
-                Check for NFT
-            </button>
-            {error && <p class="text-red-500 mb-4">{error}</p>}
-            {hasNFT && <p class="mb-4">You have an NFT!</p>}
-            {tokenId && <p class="mb-4">NFT ID: {tokenId}</p>}
-            {tokenURI && (
-                <img
-                    class="mb-4 object-contain hover:object-scale-down"
-                    src={tokenURI}
-                    alt="NFT"
-                />
-            )}
-
-            {/* Burn NFT */}
-            {hasNFT && (
-                <div>
-                    <button
-                        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4"
-                        onClick={() => handleBurn(tokenId)}
-                    >
-                        Burn
-                    </button>
-
-                    {burning && <p class="mb-4">Burning...</p>}
-                    {txHash && <p class="mb-4">Transaction hash: {txHash}</p>}
-                </div>
-            )}
-        </div>
+  {/* Check NFT */}
+  <button
+    class="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded mb-8"
+    onClick={handleCheckNFT}
+  >
+    Check for NFT
+  </button>
+  {error && <p class="text-red-500 mb-4">{error}</p>}
+  {hasNFT && <p class="mb-4">You have an NFT!</p>}
+  {tokenId && (
+    <div class="grid grid-cols-2 gap-2 mb-8">
+      <img
+        class="h-auto max-w-full rounded-lg shadow-lg"
+        src={tokenURI}
+        alt="NFT"
+      />
+      <div class="flex flex-col justify-center">
+        <p class="font-bold text-xl mb-2">NFT ID:</p>
+        <p class="mb-2">{tokenId}</p>
+        <button
+          class="py-3 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4"
+          onClick={() => handleBurn(tokenId)}
+        >
+          Burn
+        </button>
+        {burning && <p class="mb-4">Burning...</p>}
+        {txHash && <p class="mb-4">Transaction hash: {txHash}</p>}
+      </div>
+    </div>
+  )}
+</div>
+ 
     )
 }
