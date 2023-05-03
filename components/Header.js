4b2a1cbf4d005db512bb64cce1aeabcd55e31366
spo0ds <asmitadhikari540@gmail.com>
2023-05-03 09:39:17 +0545

    header fixed

--- components/Header.js
+++ components/Header.js
@@ -3,48 +3,66 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-            <h1 className="py-4 px-4 font-bold text-3xl">OxAuth</h1>
-            <div className="flex flex-row items-center">
-                <Link href="/">
-                    <a className="mr-4 p-6">Home</a>
-                </Link>
-
-                <Link href="/Kyc">
-                    <a className="mr-4 p-6">KYC</a>
-                </Link>
-
-                <Link href="/nft">
-                    <a className="mr-4 p-6">NFT</a>
-                </Link>
-                
-                <Link href="/RequestData">
-                    <a className="mr-4 p-6">Request</a>
-                </Link>
-                 <Link href="/DisplayData">
-                    <a className="mr-4 p-6">Display Data</a>
-                </Link>
-
-
-                <Link href="/ApproveData">
-                    <a className="mr-4 p-6">Approve</a>
-                </Link>
-                <Link href="/ApproveCondition">
-                    <a className="mr-4 p-6">Approve Condition</a>
-                </Link>
-                <Link href="/DecryptData">
-                    <a className="mr-4 p-6">Decrypt Data</a>
-                </Link>
-                <Link href="/StoreKyc">
-                    <a className="mr-4 p-6">Store KYC</a>
-                </Link>
-                <Link href="/UpdateData">
-                    <a className="mr-4 p-6">Update Data</a>
-                </Link>
-                <Link href="/RevokeData">
-                    <a className="mr-4 p-6">Revoke Data</a>
-                </Link>
-                <ConnectButton moralisAuth={false} />
+        <nav className="p-5 border-b-2 flex flex-wrap items-center justify-between">
+            <div className="flex items-center mr-6">
+                <h1 className="font-bold text-3xl">OxAuth</h1>
+            </div>
+            <div className="flex-grow">
+                <div className="flex items-center justify-end">
+                    <div className="flex flex-col lg:flex-row">
+                        <Link href="/">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Home
+                            </a>
+                        </Link>
+                        <Link href="/nft">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                NFT
+                            </a>
+                        </Link>
+                        <Link href="/Kyc">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                KYC
+                            </a>
+                        </Link>
+                        <Link href="/RequestData">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Request
+                            </a>
+                        </Link>
+                        <Link href="/DisplayData">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Display Data
+                            </a>
+                        </Link>
+                        <Link href="/ApproveStatus">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Approve Condition
+                            </a>
+                        </Link>
+                        <Link href="/DecryptMyData">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Decrypt Data
+                            </a>
+                        </Link>
+                        <Link href="/Approve">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Store KYC
+                            </a>
+                        </Link>
+                        <Link href="/UpdateData">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Update Data
+                            </a>
+                        </Link>
+                        <Link href="/RevokeAccess">
+                            <a className="block px-3 py-2 rounded hover:bg-gray-200 lg:mx-3 lg:my-0 my-1">
+                                Revoke Data
+                            </a>
+                        </Link>
+                    </div>
+                    <ConnectButton moralisAuth={false} className="ml-4" />
+                </div>
             </div>
         </nav>
     )
