25bd153087f5de2bc672e936ef00b0c7d93f977c
spo0ds <asmitadhikari540@gmail.com>
2023-05-03 11:18:39 +0545

    aes implemented

--- pages/DecryptData.js
+++ pages/DecryptData.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import { getContract } from "./contract"
 import { aes } from "../utils/aes"
 
@@ -10,81 +10,76 @@ export default function DecryptData() {
     const [error, setError] = useState(null)
     const [dataRequester, setDataRequester] = useState("")
     const [status, setStatus] = useState("")
-    const [decryptedData, setDecryptData] = useState("")
+    const [encryptionKey, setEncryptionKey] = useState("")
+    const [decryptedData, setDecryptedData] = useState("")
 
     const handleDecrypt = async (event) => {
         event.preventDefault()
         setError(null)
+        setDecryptedData("")
 
         try {
             const contract = await getContract()
-            const decryptedData = await contract.decryptMyData(dataProvider, data, {
+            const encryptedData = await contract.decryptMyData(dataProvider, data, {
                 gasLimit: 300000,
             })
-            setDecryptData(decryptedData)
+            console.log(`encryptedData:${encryptedData}`)
+            const decryptedData = aes.decryptMessage(encryptedData, encryptionKey)
+            setDecryptedData(decryptedData)
         } catch (err) {
             setError(err.message)
         }
     }
 
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        setStatus("Submitting to Store data.....")
-
-        try {
-            const contract = await getContract()
-            const tx = await contract.storeRsaEncryptedinRetrievable(
-                dataRequester,
-                kycField,
-                data1
-            )
-            // wait for the transaction to be confirmed and update the status
-
-            const receipt = await tx.wait()
-            setStatus(`Transaction confirmed: ${receipt.transactionHash}`)
-        } catch (error) {
-            console.error(error)
-            setStatus("Error submitting data")
-        }
-    }
     return (
         <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-            <h2 class="py-5 text-4xl font-bold dark:text-yellow">Decrypting data </h2>
-            <div class="mb-4">
-                <label class="block text-gray-700 font-bold mb-2" for="inline-full-name">
+            <h2 className="py-5 text-4xl font-bold dark:text-yellow">Decrypting data</h2>
+            <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="data-provider">
                     DataProvider address:
                 </label>
                 <input
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                    id="inline-full-name"
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="data-provider"
                     type="text"
                     value={dataProvider}
                     onChange={(e) => setDataProvider(e.target.value)}
                 />
             </div>
-            <div class="mb-4">
-                <label class="block text-gray-700 font-bold mb-2" for="inline-full-name">
-                    Data:
+            <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="data-field">
+                    Data Field:
                 </label>
                 <input
-                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                    id="inline-full-name"
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="data-field"
                     type="text"
                     value={data}
                     onChange={(e) => setData(e.target.value)}
                 />
             </div>
-            <hr class="mb-4" />
-            {error && <p class="text-red-500">{error}</p>}
+            <div className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor="encryption-key">
+                    Encryption key:
+                </label>
+                <input
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    id="inline-full-name"
+                    type="password"
+                    value={encryptionKey}
+                    onChange={(e) => setEncryptionKey(e.target.value)}
+                />
+            </div>
             <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
                 onClick={handleDecrypt}
             >
                 Decrypt Data
             </button>
-            <div>{decryptedData}</div>
-            <hr />
-            <hr />
+            <div className="my-5">
+                {decryptedData && <p class="text-xl font-bold">Decrypted Data: {decryptedData}</p>}
+                {error && <p className="text-red-500 font-bold">{error}</p>}
+            </div>
         </div>
     )
 }
